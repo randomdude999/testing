69e015c55d4d14947b9360e27ab8e1b557c38d5d
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-04-15 00:09:48 +0300

    asdf 3

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,12 @@ git config --global user.name "Travis CI"
 git add test_file_$TRAVIS_BUILD_NUMBER
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-echo Key: $encrypted_46195950b351_key
-echo IV: $encrypted_46195950b351_iv
+# echo Key: $encrypted_46195950b351_key
+# echo IV: $encrypted_46195950b351_iv
 
 openssl aes-256-cbc -K $encrypted_46195950b351_key -iv $encrypted_46195950b351_iv -in key.enc -out key -d
-ssh-add ./key
+eval $(ssh-agent -s)
+ssh-add ./key 
 
-git remote add origin ssh://git@github.com/randomdude999/testing.git
-git push --set-upstream origin master
+git remote add origin-asdf git@github.com/randomdude999/testing.git
+git push --set-upstream origin-asdf master
