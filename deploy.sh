32edc70035521c99c02e89f56c987105699d13d9
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-04-14 23:57:29 +0300

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git checkout -b master
+git add test_file_$TRAVIS_BUILD_NUMBER
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+openssl aes-256-cbc -K $encrypted_46195950b351_key -iv $encrypted_46195950b351_iv -in key.enc -out key -d
+ssh-add ./key
+
+git remote add origin https://git@github.com/randomdude999/testing.git
+git push --set-upstream origin master
